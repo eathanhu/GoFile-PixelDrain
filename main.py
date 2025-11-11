15b3dfd9d3a25cf269539d56d6aeff242e2d43e7
eathanhu <eathanhunt2023@gmail.com>
2025-11-11 19:00:56 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from pyrogram import Client
+from .config import API_ID, API_HASH, BOT_TOKEN
+from . import handlers  # registers handlers on import
+
+
+def run():
+    app = Client('gofile_pixeldrain_bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+    app.run()
+
+
+if __name__ == '__main__':
+    run()
