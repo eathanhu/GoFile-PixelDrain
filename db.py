abcd106ee85f8728b49fd8e228d108661551eab1
eathanhu <eathanhunt2023@gmail.com>
2025-12-10 18:54:47 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from config import MONGODB_URL
+from bot.config import MONGODB_URL
 
 client = MongoClient(MONGODB_URL)
 db = client['leechbot']
@@ -7,6 +7,7 @@ users = db['users']
 
 
 def get_user(user_id: int) -> dict:
+    """Get user data from database, create if doesn't exist."""
     doc = users.find_one({'user_id': user_id})
     if not doc:
         doc = {'user_id': user_id, 'gofile_token': None, 'pixeldrain_key': None}
@@ -15,4 +16,5 @@ def get_user(user_id: int) -> dict:
 
 
 def set_user_token(user_id: int, kind: str, token: str | None):
+    """Set user token (gofile_token or pixeldrain_key)."""
     users.update_one({'user_id': user_id}, {'$set': {kind: token}}, upsert=True)
